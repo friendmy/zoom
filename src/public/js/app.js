f4e20a7dad982375cfb915d16eb8406a490ee9ee
friendmy <friendmytrue@gmail.com>
2021-08-02 09:03:53 +0900

    Send Message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,28 @@
-alert("hi!")
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const handleOpen = () => {
+    console.log("Connectied to Browser ✅");
+};
+
+const handleClose = () => {
+    console.log("Disconnectied to Browser ❎");
+};
+
+const handleMessage = (message) => {
+    console.log("New message: ", message.data);
+};
+
+socket.addEventListener("open", handleOpen);
+socket.addEventListener("message", handleMessage);
+socket.addEventListener("close", handleClose);
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+};
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,19 +1,37 @@
-import http from "http"
+import http from "http";
 import WebSocket from "ws";
-import express from "express"
+import express from "express";
 
 const app = express();
 
-app.set("view engine","pug")
+app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => res.render("home"))
-app.get("/*", (req,res) => res.redirect("/"))
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+const onSocketClose = () => {
+    console.log("Disconnected from the Browser ❎");
+};
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser ✅");
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) => {
+            aSocket.send(message.toString());
+        });
+    });
+    socket.send("hello!!!");
+});
 
 server.listen(3000, handleListen);
